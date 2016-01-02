86282b13386694f5e4e8a6664f62ee3f0af3046b
yuanyangwu <yuanyang_wu@hotmail.com>
2016-01-02 09:44:44 +0800

    install mysql 5.6 automatically

--- mysql/resources/setup.sh
+++ mysql/resources/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+apt-get update
+echo mysql-server mysql-server/root_password password mysql | sudo debconf-set-selections
+echo mysql-server mysql-server/root_password_again password mysql | sudo debconf-set-selections
+apt-get install -qqy mysql-server-5.6
+
 #wget http://dev.mysql.com/get/mysql-apt-config_0.5.3-1_all.deb
 #dpkg -i mysql-apt-config*.deb
-#apt-get update
-#apt-get install -qqy mysql-server
+
+echo "Access MySQL via \'mysql -uroot -pmysql\'"
 exit 0 
