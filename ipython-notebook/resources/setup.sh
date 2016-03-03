e1b2fd2d2816a8404b0e6efea22ca0c4bbe42821
Yuanyang Wu <yuanyang_wu@hotmail.com>
2016-03-03 09:50:09 +0800

    upgrade to ipython 4 via pip

--- ipython-notebook/resources/setup.sh
+++ ipython-notebook/resources/setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -qqy ipython-notebook
+apt-get install -qqy ipython-notebook python-pip python-dev python-numpy python-pandas
 
-echo "ipython notebook --pylab inline --ip 0.0.0.0" > /home/vagrant/start-ipython-notebook.sh
+mkdir -p /home/vagrant/.pip
+cat << EOF > /home/vagrant/.pip/pip.conf
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
+
+[install]
+trusted-host=mirrors.aliyun.com
+EOF
+
+pip install notebook
+
+
+echo "jupyter notebook --ip 0.0.0.0" > /home/vagrant/start-ipython-notebook.sh
 echo "launch ipython notebook via ./start-ipython-notebook.sh"
 echo "open http://192.168.11.51:8888 in browser"
 exit 0 
