cff9d8bc4d893c7bc999cee638ed9fa9bb1ec658
Yuanyang Wu <yuanyang_wu@hotmail.com>
2018-09-04 10:43:41 +0800

    upgrade to python3

--- ipython-notebook/resources/setup.sh
+++ ipython-notebook/resources/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+sed -i.bak1 's/archive.ubuntu.com/mirrors.aliyun.com/' /etc/apt/sources.list
+sed -i.bak2 's/security.ubuntu.com/mirrors.aliyun.com/' /etc/apt/sources.list
+
 apt-get update
-apt-get install -qqy ipython-notebook python-pip python-dev python-numpy python-pandas
+
+apt-get install -y ipython3-notebook python3-pip python3-dev python3-numpy python3-pandas
 
 mkdir -p /home/vagrant/.pip
 cat << EOF > /home/vagrant/.pip/pip.conf
@@ -12,10 +16,12 @@ index-url = http://mirrors.aliyun.com/pypi/simple/
 trusted-host=mirrors.aliyun.com
 EOF
 
-pip install notebook
-
+pip3 install notebook
 
 echo "jupyter notebook --ip 0.0.0.0" > /home/vagrant/start-ipython-notebook.sh
+
+chown -R /home/vagrant
+
 echo "launch ipython notebook via ./start-ipython-notebook.sh"
 echo "open http://192.168.11.51:8888 in browser"
 exit 0 
